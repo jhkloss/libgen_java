0b4a9f31daf23152f21ad142a5322fa9b3f240ab
Jan Kloß <jan.kloss@outlook.de>
2022-03-02 16:32:35 +0100

    Init

---
+++ src/de/jkloss/Generate.java
@@ -0,0 +1,13 @@
+package de.jkloss;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.SOURCE)
+@Target({ElementType.METHOD})
+public @interface Generate
+{
+    public String ident() default "";
+}
